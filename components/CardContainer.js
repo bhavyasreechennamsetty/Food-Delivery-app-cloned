7d7d053d0a22a727fb633e68329b8aedf477413a
bhavyasreechennamsetty <141935351+bhavyasreechennamsetty@users.noreply.github.com>
2024-06-29 00:21:39 +0530

    added filter and clear buttons

--- components/CardContainer.js
+++ components/CardContainer.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 import data from "../utils/data";
 import Card from "./Restcard.js";
 import "./style2.css";
-
+const initialData=data;
 const SwiggyCard = () => {
+    const [restdata,setrestData]=useState(data);
     return (
+        <div>
+         <button id="Clear-btn" className="button" onClick={()=>{
+            setrestData(initialData);
+         }} >Clear</button>
+        <button id="Filter-btn" className="button" onClick={()=>{
+            setrestData(restdata.filter(obj=> obj.info.avgRating>4));
+        }}>Filter</button>
+        
         <div id="cardcontainer">
-            {data.map((obj) => {
-                return <Card  {...obj.info} />;
+            {restdata.map((obj) => {
+                return <Card key={obj.info.id}  {...obj.info} />;
             })}
+        </div>
         </div>
     );
 };
